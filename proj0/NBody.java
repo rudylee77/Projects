0774a3790be5c6b42215f8ab22bd7a1b97feaa3b
Rudy Lee <rudylee77@berkeley.edu>
2020-08-28 22:28:46 -0700

    completed proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -30,8 +30,8 @@ public class NBody{
 		StdDraw.enableDoubleBuffering();
 		StdDraw.setScale(-radius,radius);
 		while(time<T){
-			double[] xForces = new double[5];
-			double[] yForces = new double[5];
+			double[] xForces = new double[planets.length];
+			double[] yForces = new double[planets.length];
 			for(int i=0; i<planets.length; i++){
 				xForces[i] = planets[i].calcNetForceExertedByX(planets);
 				yForces[i] = planets[i].calcNetForceExertedByY(planets);
@@ -51,9 +51,9 @@ public class NBody{
 		StdOut.printf("%d\n", planets.length);
 		StdOut.printf("%.2e\n", radius);
 		for (int i = 0; i < planets.length; i++) {
-    		StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
-                  planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
-                  planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
-		}
+		    StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+		                  planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+		                  planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+		}   
 	}
 }
