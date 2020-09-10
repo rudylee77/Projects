d1c2e2fa0a086e19053d86db66eb396f66bf4dbc
Rudy Lee <rudylee77@berkeley.edu>
2020-09-09 22:09:30 -0700

    done

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,47 @@
+import java.util.LinkedList;
+
+public class LinkedListDeque{
+    public T item;
+    public LinkedListDeque next;
+
+    public LinkedListDeque(){
+        item = null;
+        next = null;
+    }
+
+    public void addFirst(T item){
+
+    }
+
+    public void addLast(T item){
+
+    }
+
+    public boolean isEmpty(){
+
+    }
+
+    public int size(){
+
+    }
+
+    public void printDeque(){
+
+    }
+
+    public T removeFirst(){
+
+    }
+
+    public T removeLast(){
+
+    }
+
+    public T get(int index){
+
+    }
+
+    public T getRecursive(int index){
+
+    }
+}
