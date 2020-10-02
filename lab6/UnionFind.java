3ee322228bd35e9ea4e1a0e55f687fc6dfd4e00e
Rudy Lee <rudylee77@berkeley.edu>
2020-10-02 04:38:13 -0700

    done

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -36,8 +36,6 @@ public class UnionFind {
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean isConnected(int v1, int v2) {
-        validate(v1);
-        validate(v2);
         return find(v1) == find(v2);
     }
 
@@ -47,9 +45,7 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void connect(int v1, int v2) {
-        validate(v1);
-        validate(v2);
-        if (isConnected(v1, v2)) {
+        if (isConnected(v1, v2) && find(v1) != find(v2)) {
             if (sizeOf(v1) > sizeOf(v2)) {
                 parent[find(v2)] = find(v1);
             } else {
@@ -61,7 +57,6 @@ public class UnionFind {
     /* Returns the root of the set v1 belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int v1) {
-        validate(v1);
         if (parent[v1] == -1) {
             return v1;
         }
