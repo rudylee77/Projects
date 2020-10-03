23ada785e3015b80245f59d22a19f5abecea65f0
Rudy Lee <rudylee77@berkeley.edu>
2020-10-02 21:11:58 -0700

    done

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -3,6 +3,10 @@ package hw2;
 public class PercolationStats {
     private double[] sum;
     private int T;
+    private double mean;
+    private double stddev;
+    private double confidenceLow;
+    private double confidenceHigh;
 
     public PercolationStats(int N, int T, PercolationFactory pf) {
         if (N <= 0 || T <= 0) {
@@ -25,18 +29,22 @@ public class PercolationStats {
             }
             sum[i] = (test.numberOfOpenSites()) / (N * N);
         }
+        mean = edu.princeton.cs.algs4.StdStats.mean(sum);
+        stddev = edu.princeton.cs.algs4.StdStats.stddev(sum);
+        confidenceLow = mean - ((1.96 * stddev) / Math.sqrt(T));
+        confidenceHigh = mean + ((1.96 * stddev) / Math.sqrt(T));
     }
 
     public double mean() {
-        return edu.princeton.cs.algs4.StdStats.mean(sum);
+        return mean;
     }
     public double stddev() {
-        return edu.princeton.cs.algs4.StdStats.stddev(sum);
+        return stddev;
     }
     public double confidenceLow() {
-        return mean() - ((1.96 * stddev()) / Math.sqrt(T));
+        return confidenceLow;
     }
     public double confidenceHigh() {
-        return mean() + ((1.96 * stddev()) / Math.sqrt(T));
+        return confidenceHigh;
     }
 }
