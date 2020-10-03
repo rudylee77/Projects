d61ccb61d4555ad2a72b8f6d39681a6b58af4447
Rudy Lee <rudylee77@berkeley.edu>
2020-10-02 23:35:37 -0700

    done

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -3,10 +3,6 @@ package hw2;
 public class PercolationStats {
     private double[] sum;
     private int T;
-    private double mean;
-    private double stddev;
-    private double confidenceLow;
-    private double confidenceHigh;
 
     public PercolationStats(int N, int T, PercolationFactory pf) {
         if (N <= 0 || T <= 0) {
@@ -16,7 +12,6 @@ public class PercolationStats {
         sum = new double[T];
         for (int i = 0; i < T; i++) {
             Percolation test = pf.make(N);
-            int count = 0;
             while (!(test.percolates())) {
                 boolean isOpen = true;
                 int row = 0;
@@ -27,26 +22,21 @@ public class PercolationStats {
                     isOpen = test.isOpen(row, col);
                 }
                 test.open(row, col);
-                count++;
             }
-            sum[i] = count / (N * N);
+            sum[i] = (double) (test.numberOfOpenSites() / (N * N));
         }
-        mean = edu.princeton.cs.algs4.StdStats.mean(sum);
-        stddev = edu.princeton.cs.algs4.StdStats.stddev(sum);
-        confidenceLow = mean - ((1.96 * stddev) / Math.sqrt(T));
-        confidenceHigh = mean + ((1.96 * stddev) / Math.sqrt(T));
     }
 
     public double mean() {
-        return mean;
+        return edu.princeton.cs.algs4.StdStats.mean(sum);
     }
     public double stddev() {
-        return stddev;
+        return edu.princeton.cs.algs4.StdStats.stddev(sum);
     }
     public double confidenceLow() {
-        return confidenceLow;
+        return mean() - ((1.96 * stddev()) / Math.sqrt(T));
     }
     public double confidenceHigh() {
-        return confidenceHigh;
+        return mean() + ((1.96 * stddev()) / Math.sqrt(T));
     }
 }
